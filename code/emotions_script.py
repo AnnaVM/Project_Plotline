f71b3840419003681cad1f38d7be082b8244fd3f
AnnaVM <anna.venanciomarques@gmail.com>
2016-03-20 16:29:27 -0700

    Added the emotion script

---
+++ code/emotions_script.py
@@ -0,0 +1,192 @@
+'''
+this script gets the emotion counts in the movie scripts contained in the file
+'../data/new_scraping/texts/'
+
+the progression of emotion counts can be followed on the terminal
+
+this is based on the lexicon NRC_emotions.txt (placed in '../data/emotions/')
+--> for more information on the NRC Word-Emotion Association Lexicon, visit
+    'http://saifmohammad.com/WebPages/NRC-Emotion-Lexicon.htm'
+
+8 emotions covered + positive/negative sentiment: anger, anticipation, disgust,
+    fear, joy, negative, positive, sadness, surprise, trust
+
+Usage:
+------
+To execute this script, type in a terminal
+$ python emotions_script.py
+
+Challenges --> choices:
+-----------------------
+increasing speed of code: using arrays, faster look up in the dataframe
+
+Files created:
+--------------
+in ../data/emotions/arrays folder: all of the emotion counts available in .npy
+'''
+
+import pandas as pd
+import numpy as np
+import os
+from plotline_utilities import progression_bar
+
+from nltk.stem import WordNetLemmatizer
+
+def load_dictionary_and_vocabulary(filename):
+    '''
+    parameters:
+    -----
+    filename to the word-emotion association lexicon
+
+    returns:
+    --------
+    dictionary with word as index and 0/1 value for related emotion in columns
+    (anger, anticipation, disgust, fear, joy, negative,
+    positive, sadness, surprise, trust)
+    set of vocabulary, ie all the words that appear in the dictionary
+
+    '''
+    # Load the NRC emotions
+    emotion_df = pd.read_csv(filename,
+                     delim_whitespace=True, header=None)
+    reshaped_df = emotion_df.pivot( 0, 1, 2 )
+
+    # Get the list of existing words
+    vocabulary = set( emotion_df[0].unique() )
+
+    # Convert to dictionary
+    emotion_dict = {}
+    for word in list( vocabulary ):
+        emotion_dict[word] = reshaped_df.ix[ word ].values
+    return emotion_dict, vocabulary
+
+#######text clean-up (lemmatize, lowercase)
+def get_clean_text(list_filenames, path_to_file):
+    '''
+    parameter:
+    ----------
+    list_filenames: as LST is a list of filename as STR
+    path_to_file: as STR is the path to the file containing movie scripts
+    --> such that path_to_file/filename.txt is the file to open
+
+    returns:
+    --------
+    list of list of words (lemmatize, lowercase) in the text (order preserved)
+    '''
+    wnl = WordNetLemmatizer()
+    list_texts_as_words = []
+    for filename in list_filenames:
+        path_file = path_to_file+"/"+filename+".txt"
+        with open(path_file) as f:
+            text = f.readlines()
+            lines = [line.strip() for line in text if line.strip()]
+            string_words = []
+            for line in lines:
+                words = [wnl.lemmatize(word.lower()) for word in line.split(' ') if wnl.lemmatize(word.lower())]
+                string_words += words
+        list_texts_as_words.append(string_words)
+    return list_texts_as_words
+
+#######get chunks of text
+def window_blocks(text, size_block=100):
+    '''
+    parameters:
+    -----------
+    text: list of words in a textfile
+    size_block: size of the window (how many consecutive words to
+                add to a window)
+
+    returns:
+    --------
+    the text as a list of windows (window = list of words of length size_block)
+    '''
+    list_windows = []
+    for i in xrange(0,len(text)-size_block,size_block):
+        window = text[i:i+size_block]
+        list_windows.append(window)
+    return list_windows
+
+def emotion_counts(list_words, df, vocabulary):
+    '''
+    parameters:
+    -----------
+    list of words (typically 100 words long)
+    df: from NRC emotions, of shape (word as index, emotions as column names)
+    vocabulary: list of unique words for which we have emotions
+                vocabulary = df[0].unique()
+
+    returns:
+    --------
+    emotion_counts: array with the emotion count for the 8 emotions +
+                    positive/negative sentiment
+    '''
+    # Initializing an array of 8 emotions + 2 sentiments (positive/negative).
+    # Order:
+    # anger, anticipation, disgust, fear, joy, negative,
+    # positive, sadness, surprise, trust
+    emotion_count = np.zeros(10)
+
+    #counting the number of words in each emotion
+    for word in list_words:
+        if word in vocabulary:
+            #looking up the word
+            emotion_for_word = df[ word ]
+            # Adding up the emotions
+            emotion_count += emotion_for_word
+
+    return emotion_count
+
+def get_emotions(filename, path_to_file, df, vocabulary,
+                 print_to_file=False,
+                 verbose=False):
+    '''
+    parameters:
+    -----------
+    filename: as STR
+    path_to_file: as STR
+        such that 'path_to_file/filename.txt' is the script being analyzed
+    df: dictionary with word as index and 0/1 in emotion columns (see
+        load_dictionary_and_vocabulary function)
+    vocabulary: all the words from the previous dictionary
+    print_to_file: BOOL gives the option to save the counts as array in
+                    ../data/emotions/arrays/filename.npy
+    verbose: BOOL that prints progress (every 10 windows treated)
+
+    returns:
+    --------
+    '''
+    text = get_clean_text([filename], path_to_file)[0]
+    list_windows = window_blocks(text, size_block=100)
+    if verbose:
+        print 'New file:', len(list_windows)
+    list_emotions=[]
+    index = 0
+    for window in list_windows:
+        index +=1
+        list_emotions.append( emotion_counts(window, df , vocabulary) )
+        if index%10==0 and verbose:
+            print index
+    array_emotions = np.array(list_emotions)
+    if print_to_file:
+        path_to_file = "../data/emotions/arrays/"+filename
+        np.save(path_to_file, array_emotions)
+    return array_emotions
+
+if __name__ == '__main__':
+    NRC_emotions_file = '../data/emotions/NRC_emotions.txt'
+    emotion_dictionary, vocabulary = load_dictionary_and_vocabulary(NRC_emotions_file)
+
+    # Create the proper directories
+    if not os.path.exists('../data/emotions/arrays'):
+        os.mkdir('../data/emotions/arrays') 
+
+    # Loop through the script files
+    path_to_file = '../data/scraping/texts'
+    files = os.listdir(path_to_file)
+    index = 0
+    Ntot = len(files)
+    for filename in files[::-1]:
+        get_emotions( filename[:-4], path_to_file, emotion_dictionary, vocabulary,\
+                      print_to_file=True, verbose=False)
+        progression_bar(index, Ntot, Nbars=60, char='-')
+        index += 1
